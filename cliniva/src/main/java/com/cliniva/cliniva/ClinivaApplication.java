0c921baac5c456d25337ed28b6ed392cad371e26
Paulo Victor Pinheiro <pauovictorpinheiro998663264@gmail.com>
2026-08-07 18:29:27 -0300

    chore: initialize Spring Boot project

---
+++ cliniva/src/main/java/com/cliniva/cliniva/ClinivaApplication.java
@@ -0,0 +1,13 @@
+package com.cliniva.cliniva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinivaApplication.class, args);
+	}
+
+}
---
+++ cliniva/src/test/java/com/cliniva/cliniva/ClinivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cliniva.cliniva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
