7772fafd22063f3beb1cb696657d40431d3f02c1
ADMITO-MITO <pauovictorpinheiro998663264@gmail.com>
2026-08-15 09:47:36 -0300

    feat(atendimento): add domain
    add querys in repository

--- cliniva/src/main/java/com/cliniva/atendimento/AtendimentoRepository.java
+++ cliniva/src/main/java/com/cliniva/atendimento/AtendimentoRepository.java
@@ -1,9 +1,17 @@
 package com.cliniva.atendimento;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDateTime;
+import com.cliniva.atendimento.enums.StatusAtendimento;
 
 public interface AtendimentoRepository extends JpaRepository<Atendimento, UUID> {
 
+    List<Atendimento> findByDataAtendimentoBetween(LocalDateTime inicio, LocalDateTime fim);
+
+    List<Atendimento> findByStatus(StatusAtendimento status);
+
+    List<Atendimento> findByCliente_Id(UUID clienteId);
 }
