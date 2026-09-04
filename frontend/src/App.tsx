9ee4ce05960b66b0187ba3a7e247ba5cf51fabb3
ADMITO-MITO <pauovictorpinheiro998663264@gmail.com>
2026-09-04 16:43:54 -0300

    feat: integrate clientelist to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,15 @@
+import ClienteList from "./ClienteList"
 function App() {
-
   const cliente = [
     { id: 1, nome: "Maria Silva" },
     { id: 2, nome: "João Souza" },
     { id: 3, nome: "Ana Costa" }
   ]
-
-  return <>
+  return <div style={{ padding: "20px" }}>
     <h1>Cliniva</h1>
     <section>
-      <ul>
-        {cliente.map((c) => (
-          <li key={c.id}>{c.nome}</li>
-        ))}
-      </ul>
+      <ClienteList clientes={cliente} />
     </section>
-
-  </>
-
-
+  </div>
 }
-
 export default App
