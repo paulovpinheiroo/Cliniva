d08b35c6a0764f3c0d6a18d4a0571acbda4bc785
ADMITO-MITO <pauovictorpinheiro998663264@gmail.com>
2026-09-04 17:25:04 -0300

    feat: add on remove logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import ClienteList from "./ClienteList"
 function App() {
-  const cliente = [
+
+  const [cliente, setCliente] = useState([
     { id: 1, nome: "Maria Silva" },
     { id: 2, nome: "João Souza" },
     { id: 3, nome: "Ana Costa" }
-  ]
+  ]);
+  const proximoId = cliente.length > 0 ? Math.max(...cliente.map(c => c.id)) + 1 : 1;
+  const adicionarCliente = () => {
+    const novoCliente = { id: proximoId, nome: "Novo Cliente" };
+    setCliente([...cliente, novoCliente]);
+  };
+  const handleRemoverCliente = (idParaRemover: number) => {
+    const listaAtualizada = cliente.filter(cliente => cliente.id !== idParaRemover);
+    setCliente(listaAtualizada);
+  };
+
+
   return <div style={{ padding: "20px" }}>
     <h1>Cliniva</h1>
     <section>
-      <ClienteList clientes={cliente} />
+      <ClienteList clientes={cliente} onRemover={handleRemoverCliente} />
     </section>
+    <button onClick={adicionarCliente}>Adicionar Cliente</button>
   </div>
 }
 export default App
